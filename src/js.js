6f8afeaa5c05643e32d9bdb7503e30f6d2eeaf2e
Mihael Mihaljević <git@mihael.mihaljevic.from.hr>
2021-11-20 12:25:53 +0100

    Move to Vue

--- src/js.js
+++ src/js.js
@@ -1,47 +1,22 @@
-let colorMode = "light";
-
-const ELEMENTS = {
-  colorModeToggler: document.getElementById("color-mode-toggler"),
-};
-
-init();
-
-function init() {
-  ELEMENTS.colorModeToggler.addEventListener("click", () => {
-    toggleColorMode();
-    document.activeElement && document.activeElement.blur();
-  });
-  ELEMENTS.colorModeToggler.addEventListener("keypress", toggleColorMode);
-
-  detectColorModePreference();
-  activateColorMode();
-}
-
-function detectColorModePreference() {
-  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    colorMode = "dark";
-  }
-}
-
-function toggleColorMode() {
-  colorMode = colorMode === "light" ? "dark" : "light";
-  activateColorMode();
-}
-
-function activateColorMode() {
-  if (colorMode === "light") {
-    document.body.classList.remove("dark");
-    ELEMENTS.colorModeToggler
-      .querySelector("#dark-mode")
-      .removeAttribute("style");
-    ELEMENTS.colorModeToggler.querySelector("#light-mode").style.display =
-      "none";
-  } else {
-    document.body.classList.add("dark");
-    ELEMENTS.colorModeToggler
-      .querySelector("#light-mode")
-      .removeAttribute("style");
-    ELEMENTS.colorModeToggler.querySelector("#dark-mode").style.display =
-      "none";
-  }
-}
+const asideMenu = new Vue({
+  el: "aside",
+  data: {
+    isDarkMode: null,
+  },
+  methods: {
+    toggleColorMode() {
+      this.isDarkMode = !this.isDarkMode;
+    },
+  },
+  watch: {
+    isDarkMode(val) {
+      return val
+        ? document.body.classList.add("dark")
+        : document.body.classList.remove("dark");
+    },
+  },
+  mounted() {
+    this.isDarkMode = !!window.matchMedia("(prefers-color-scheme: dark)")
+      .matches;
+  },
+});
