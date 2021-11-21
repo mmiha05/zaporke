4558ed406ecdad07c49231e1ef73482beac09ee3
Mihael Mihaljević <git@mihael.mihaljevic.from.hr>
2021-11-21 21:22:05 +0100

    Change docs

--- scripts/build.js
+++ scripts/build.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 const SRC_DIR = path.join(__dirname, "..", "src");
-const PUBLIC_DIR = path.join(__dirname, "..", "public");
+const PUBLIC_DIR = path.join(__dirname, "..", "docs");
 
 const filesToCopy = ["favicon.ico", "index.html", "styles.css", "words.js"];
 
